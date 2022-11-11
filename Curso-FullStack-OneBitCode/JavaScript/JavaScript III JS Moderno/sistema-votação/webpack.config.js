c7fa822ca64955196232543d34302b12113bb53a
Renan Gatti <renanfrancisco96@gmail.com>
2022-11-11 11:18:21 -0300

    webpack

---
+++ Curso-FullStack-OneBitCode/JavaScript/JavaScript III JS Moderno/sistema-votação/webpack.config.js
@@ -0,0 +1,22 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: "./src/vote.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].min.js",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+};
