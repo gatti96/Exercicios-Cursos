ce94f070864a0aee5e20a2842f17360bfe837233
Renan Gatti <renanfrancisco96@gmail.com>
2022-11-11 17:03:14 -0300

    yes

--- Curso-FullStack-OneBitCode/JavaScript/JavaScript III JS Moderno/sistema-votação/src/vote.js
+++ Curso-FullStack-OneBitCode/JavaScript/JavaScript III JS Moderno/sistema-votação/src/vote.js
@@ -1,4 +1,8 @@
 const password = "123";
+let yes = 0;
+let not = 0;
+let nule = 0;
+let electore = 0;
 
 document.getElementById("init").addEventListener("click", function () {
   const senha = document.getElementById("password").value;
@@ -9,4 +13,23 @@ document.getElementById("init").addEventListener("click", function () {
   } else {
     alert("Senha incorreta");
   }
+});
+
+document.getElementById("yes").addEventListener("click", function () {
+  yes = yes + 1;
+  electore = electore + 1;
+  const confirm = document.getElementById("confirm");
+  confirm.value = "voto computado";
+  confirm.style.backgroundColor = "green";
+  confirm.style.color = "#fff";
+});
+
+document.getElementById("not").addEventListener("click", function () {
+  not = not + 1;
+  electore = electore + 1;
+});
+
+document.getElementById("nule").addEventListener("click", function () {
+  nule = nule + 1;
+  electore = electore + 1;
 });
