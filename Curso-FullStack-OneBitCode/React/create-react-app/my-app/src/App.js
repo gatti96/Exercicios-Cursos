a29e8d9d3d6ca5bb06f0a956142f2b9fcf8f752c
Renan Gatti <renanfrancisco96@gmail.com>
2023-02-01 09:34:48 -0300

    react

--- Curso-FullStack-OneBitCode/React/create-react-app/my-app/src/App.js
+++ Curso-FullStack-OneBitCode/React/create-react-app/my-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Loren from "./components/lorem";
 
 function HelloWorld() {
   return <h1>Hello World</h1>;
@@ -8,24 +9,14 @@ function HelloWorld() {
 function App() {
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
       <div>
         <HelloWorld />
+      </div>
+      <div>
+        <br></br>
+      </div>
+      <div>
+        <Loren />
       </div>
     </>
   );
---
+++ Curso-FullStack-OneBitCode/React/create-react-app/my-app/src/components/lorem.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class Loren extends React.Component {
+  render() {
+    return (
+      <p>
+        Praesent pellentesque odio ut lacus imperdiet dignissim. Donec vitae
+        ultricies nisl. Nam ut rhoncus elit, eu condimentum lectus. Praesent at
+        nibh ut lorem fringilla pharetra. Donec pretium pretium felis a
+        ullamcorper. Nulla et diam nisl. Etiam euismod et orci in maximus. Proin
+        eget mattis sapien. Mauris pharetra justo nec velit placerat lobortis.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia curae; Donec nec nunc rutrum sem fermentum mollis. Praesent
+        pharetra gravida libero id iaculis. Etiam ornare interdum luctus.
+        Aliquam eu enim blandit, convallis eros sit amet, venenatis est. Etiam
+        laoreet tellus dolor, sit amet pellentesque lacus congue ut. Mauris
+        suscipit neque enim, non vulputate odio mollis quis.
+      </p>
+    );
+  }
+}
+
+export default Loren;
